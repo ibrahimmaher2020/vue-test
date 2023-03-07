a8f1882361aa94f21ba4f6e51e01585e7775593a
ibrahimmaher2020 <51016531+ibrahimmaher2020@users.noreply.github.com>
2023-03-07 11:51:03 +0200

    initial commit

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,110 @@
+<template>
+  <form action="post">   
+    <label >Email:</label>
+    <input type="email" required v-model="email">
+    <label >Password:</label>
+    <input type="password" required v-model="password">
+    <label >Role:</label>
+    <select v-model="role">
+        <option value="Developer">Web Developer</option>
+        <option value="Designer">Web designer</option>
+    </select>
+    <label >Skills -
+        <p style="color:brown">  Tyep <strong> ',' </strong> to add text -</p>
+    </label>
+    <input type="text" v-model="tempSkills" @keyup="addSkill">
+    
+    <div v-for="skill in skills" :key="skill" class="pill">
+        
+          {{skill}}
+        
+    </div>
+
+    <div class="terms">
+        <input type="checkbox" v-model="terms" required>
+        <label >Accept terms and condtions</label>       
+    </div>
+   
+    
+  </form>
+  <p>email : {{ email  }}</p>
+  <p>password : {{ password  }}</p>
+  <p>Role : {{ role  }}</p>
+  <p>terms : {{ terms}}</p>  
+
+</template>
+
+<script>
+
+
+export default {
+    
+    data(){
+        return{
+            email:'ibrahim',
+            password:'',
+            role:'Designer',
+            terms:false,  
+            tempSkills:'',
+            skills:[],
+        }
+    },
+    methods:{
+        addSkill(e){
+
+            if (e.key===',' && this.tempSkills) {
+                this.tempSkills=this.tempSkills.substring(0,this.tempSkills.length-1)//delete Comma
+                
+                if (!this.skills.includes(this.tempSkills)){
+                    //this.skills.push(this.tempSkills.substring(0,this.tempSkills.length-1))                    
+                    this.skills.push(this.tempSkills)                    
+                }
+                console.log(e);
+                this.tempSkills=''
+            }
+            
+            
+        }
+    }
+    
+}
+</script>
+
+<style>
+form{
+
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;    
+}
+label{
+    color:#aaa;
+    display:inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.9em;
+    text-transform: capitalize;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+input,select {
+    display: block;
+    padding: 10px 6px;
+    width:100%;
+    box-sizing: border-box;
+    border:none;
+    border-bottom: 1px solid #ddd;
+    color:#555;
+}
+input[type="checkbox"]{
+    display: inline-block;
+    width:16px;
+    margin : 0 10px 0 0;
+    position:relative;
+    top:2px;
+
+}
+
+</style>
